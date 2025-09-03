408ac01a835d86c17f29d46f9c43a6fc35d3b9a2
morteza <52275275+MoriNoise@users.noreply.github.com>
2025-09-03 14:23:25 +0330

    better dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,41 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Admin;
+use App\Models\MessageGroup;
 use App\Models\MessageTemplate;
 use App\Models\User;
 
 
 class DashboardController extends Controller
 {
+
     public function index()
     {
-//        $orders = Order::query()
-//            ->with(['user', 'items.product'])
-//            ->latest()
-//            ->limit(10)
-//            ->paginate(10);
-//        $recentTransactions = OrderItem::with(['order.user', 'product'])
-//            ->latest()
-//            ->take(10)
-//            ->get();
-//
-//        $totalSalesCount = Order::count();
-//        $totalRevenue = Order::sum('total_price');
-//        $totalProfit = Order::sum('total_price') * 20 / 100;
-
-
-        $totalCustomers = User::count();
+        $totalUsers = User::count();
+        $totalAdmins = Admin::count();
         $totalTemplates = MessageTemplate::count();
+        $totalGroups = MessageGroup::count();
+        $recentUsers = User::latest()->take(5)->get();
 
-        return view('admin.index', [
-            'title' => 'داشبورد',
-//            'orders' => $orders,
-//            'recentTransactions' => $recentTransactions,
-            'totalSalesCount' => 10,
-            'totalRevenue' => 20,
-            'totalTemplates' => 30,
-            'totalCustomers' => $totalCustomers,
-        ]);
+        return view('admin.index', compact(
+            'totalUsers',
+            'totalAdmins',
+            'totalTemplates',
+            'totalGroups',
+            'recentUsers'
+        ));
     }
+
 }
 
